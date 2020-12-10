48b785394bf30cdad90f91e20173ec12d6ececf5
Kerem ŞAHİN <keremsahin1905@gmail.com>
2020-12-10 19:12:45 +0300

    Model Classes

---
+++ BookUrBookModelClasses/src/PostTitleComparator.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class PostTitleComparator implements Comparator<Post>
+{
+    @Override
+    public int compare(Post o1, Post o2)
+    {
+        if (o1.getTitle().compareTo(o2.getTitle()) > 0)
+            return 1;
+        else if (o1.getTitle().compareTo(o2.getTitle()) == 0 )
+            return 0;
+        else
+            return -1;
+    }
+}
