0da30c51893ddc75f539ba7177f736a9f837490c
Kerem ŞAHİN <keremsahin1905@gmail.com>
2020-12-10 19:09:38 +0300

    Model Classes IntelliJ Project

---
+++ ModelClassesIntelliJ/src/UserSpecPostList.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class UserSpecPostList extends PostList
+{
+	//instance variables
+	private User owner;
+
+	public UserSpecPostList(User owner) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	public void updateUserSpecPostList(PostList postList)
+	{
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+}
