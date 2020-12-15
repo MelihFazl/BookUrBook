c1c1b50c0f4d1731da4d1c7c31bf8f8b7224fdee
Melih Fazıl Keskin <melihfazlkeskin@gmail.com>
2020-12-15 23:43:16 +0300

    Model classes of Chat System again
    An error has occurred for the previous commit.

--- BookUrBook/app/src/main/java/com/example/bookurbook/models/Chat.java
+++ BookUrBook/app/src/main/java/com/example/bookurbook/models/Chat.java
@@ -1,5 +1,87 @@
 package com.example.bookurbook.models;
 
+import java.util.ArrayList;
+
 public class Chat
 {
+    //variables
+    User user1;
+    User user2;
+    ArrayList<Message> messages;
+    boolean user1Blocked;
+    boolean user2Blocked;
+
+    //constructor
+    public Chat(User user1, User user2, boolean user1Blocked, boolean user2Blocked)
+    {
+        this.user1 = user1;
+        this.user2 = user2;
+        messages = new ArrayList<Message>();
+        this.user1Blocked = user1Blocked;
+        this.user2Blocked = user2Blocked;
+    }
+
+    //methods
+    public void setMessageList( ArrayList<Message> messageList )
+    {
+        messages = messageList;
+    }
+
+    public ArrayList<Message> getMessages()
+    {
+        return messages;
+    }
+
+    public void addMessage(Message msg )
+    {
+        messages.add(msg);
+    }
+
+    public Message getLastMessage()
+    {
+        if (messages.size() > 1)
+        {
+            return messages.get(messages.size() - 1);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public int isConsist( User user )
+    {
+        if ( user1.getUsername().equals(user.getUsername()) )
+        {
+            return 1;
+        }
+        else if ( user2.getUsername().equals(user.getUsername()) )
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public boolean isUser1Blocked()
+    {
+        return user1Blocked;
+    }
+
+    public boolean isUser2Blocked()
+    {
+        return user2Blocked;
+    }
+
+    public User getUser1()
+    {
+        return user1;
+    }
+
+    public User getUser2()
+    {
+        return user2;
+    }
 }
