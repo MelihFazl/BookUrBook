d60599771cc416b2e0f2a76578cc4b6540f70afd
Kaan Tek <kaantek06@gmail.com>
2020-12-20 03:24:36 +0300

    Latest updates with Post List class has been added
    Toolbar and filter screen has been added

--- BookUrBook/app/src/main/java/com/example/bookurbook/PostListActivity.java
+++ BookUrBook/app/src/main/java/com/example/bookurbook/PostListActivity.java
@@ -45,7 +45,7 @@ import java.util.ArrayList;
 public class PostListActivity extends AppCompatActivity implements FilterScreenView.FilterScreenListener {
 
     // variables
-    //Toolbar toolbar;
+    Toolbar toolbar;
     RecyclerView recyclerView;
     FilterScreenView filterScreenView;
     SearchView searchView;
@@ -55,6 +55,7 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
     Button ZtoAbutton;
     Button resetButton;
     ImageButton filterButton;
+    ImageButton createPostButton;
     PostListAdapter postListAdapter;
     PostList postList;
     private FirebaseFirestore db;
@@ -67,11 +68,8 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
 
-        // postList = new ArrayList<>();                    delete later?
-        //add();  // method for adding posts from database ?
 
-        // list from the model class
-
+        // for database purposes
         db = FirebaseFirestore.getInstance();
         if(getIntent().getSerializableExtra("currentUser") instanceof Admin)
             currentUser = (Admin)getIntent().getSerializableExtra("currentUser");
@@ -84,14 +82,10 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
         }
 
 
-        // later do all these operations with database
-       //postList.addPost(new Post("great book", "big Java", "Bilkent University", "CS", 30, null, new RegularUser("Kaan", "mail", null)));
-
-
-
+        createPostButton = findViewById(R.id.createPostButton);
         searchView = findViewById(R.id.search_id);
         recyclerView = findViewById(R.id.recycler_id);
-        //toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         LtoHpriceButton = findViewById(R.id.LtoH_price_button);
         HtoLpriceButton = findViewById(R.id.HtoL_price_button);
         AtoZbutton = findViewById(R.id.AtoZ_button);
@@ -99,17 +93,24 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
         resetButton = findViewById(R.id.reset_button);
         filterButton = findViewById(R.id.filterButton);
 
-        //setSupportActionBar(toolbar);
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);   // implement this later so that it goes back to the previous screen
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   // implement this later so that it goes back to the previous screen
 
 
         postListAdapter = new PostListAdapter(this, postList);
         recyclerView.setAdapter(postListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // filterScreenView = new FilterScreenView(postListAdapter);            may deletee?
 
         search(postListAdapter); // method for searching
+
+        createPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PostListActivity.this, CreatePostActivity.class);
+                startActivity(intent);
+            }
+        });
 
         LtoHpriceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,42 +158,12 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
 
     }
 
-    /*@Override
-    public void onBackPressed() {
-        Intent pass = new Intent(PostListActivity.this, MainMenuActivity.class);
-        pass.putExtra("user", currentUser);
-        startActivity(pass);
-    }*/
-
-
     public void openFilterWindow() {
         FilterScreenView filterScreen = new FilterScreenView();    //deleted the parameter constructor?
         filterScreen.show(getSupportFragmentManager(), "example filter");
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        /*getMenuInflater().inflate(R.menu.menu, menu); */
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        /*if (id == R.id.a)
-        {
-            // create post screen i aç
-        }*/
-        return true;
-    }
-    // method for adding posts to the post list
-
-    public void add()    // later change this method to add posts from database ???
-    {
-        // napacaz bunu
-    }
 
     // method for searching by a keyword from the list
     public void search(PostListAdapter adp)
@@ -204,16 +175,15 @@ public class PostListActivity extends AppCompatActivity implements FilterScreenV
             public boolean onQueryTextSubmit(String s) {
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String s) {
                 adapter.getFilter().filter(s);
                 return false;
             }
         });
-
     }
 
+    // this does not work ????
     @Override
     public void onBackPressed() {
         Intent pass = new Intent(PostListActivity.this, MainMenuActivity.class);
